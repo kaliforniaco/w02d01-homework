022bbb13e623a3ef2f431ea31fe955dd0263c514
David Epps <kaliforniaco@gmail.com>
2018-06-19 08:02:56 -0600

    1st commit - selecting datatypes

---
+++ js/answer.js
@@ -0,0 +1,46 @@
+console.log("ready..");
+
+//1. A light switch that can be either on or off. =BOOLEAN
+	let switchOn=true;
+//2. A user's email address. =STRING
+	const email="linxkynd@gmail.com";
+//3. A spaceship with a hull, laser blasters, tractor beam, and warp drive. =OBJECT
+	const heartOfGold = {
+		passengers: 7,
+		computer: "Eddie";
+		improbabilityDrive: true,
+		possibleInstances: Infinite
+	}
+
+//4. A list of student names from our class. =ARRAY
+	let classList=["David", "Elizabeth", "Bowen", "Riley"]
+
+//5. A list of student names from our class, each with a location. =ARRAY
+	const Roster=[
+	["David", "Evergreen"],
+	["Liz", "Kittredge"],
+	["Bowen", "Lakewood"],
+	["Riley", "Sheridan"]
+]
+
+//6. A list of student names from our class, each with a location and each with a list of favorite tv shows. =OBJECT
+	const classListDetailed = {
+		{
+			name: "David",
+			location: "Evergreen",
+			favoriteMovies: ["Princess Bride","TRON", "Labyrinth"]
+		}
+		{	name: "Liz",
+			location: "Kittredge",
+			favoriteMovies: ["New Year's Eve", "Valentine's Day"]
+		}
+		{
+			name: "Bowen",
+			location: "Lakweood",
+			favoriteMovies: ["Deadpool", "Ready Player One", "Maze Runner"]
+		}
+
+
+
+
+
