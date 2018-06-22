d69f6a965ac9ca5124c219651b98f31f386715c3
David Epps <kaliforniaco@gmail.com>
2018-06-22 16:12:36 -0600

    4th commit - accessed limbo

--- js/answer.js
+++ js/answer.js
@@ -89,6 +89,30 @@ const crazyObject = {
 console.log(crazyObject.taco[1].salsa[5]);
 console.log(crazyObject.larry.quotes[0]);
 console.log(crazyObject.larry.characters[2].favourtieHobby);
+console.log(crazyObject.larry.nicknames[1]);
+console.log(crazyObject.larry.characters[1].name);
 
+//-
 
+//Object-ception
 
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo=null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
